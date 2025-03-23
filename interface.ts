2f1a054d549cabae6c92621264178f4fe49f5da2
ApiwitJoey <joeyapiwitch@gmail.com>
2025-03-23 19:31:03 +0700

    Add interface

--- interface.ts
+++ interface.ts
@@ -1,3 +1,5 @@
+import { Date, ObjectId } from "mongoose"
+
 export interface CampgroundItem {
     id : string
     name: string
@@ -10,4 +12,18 @@ export interface CampgroundItem {
 export interface CampgroundJson {
     count : number
     data : CampgroundItem[]
+}
+
+export interface ReservationItem {
+    rDate : Date
+    user : ObjectId
+    campground : ObjectId
+    createdAt : Date
+}
+
+export interface LogItem {
+    action : string
+    user : ObjectId
+    reserve : ObjectId
+    timestamp : Date
 }
